ed196c3adf7721b76f491a263ec525da2da766f0
Sri Harsha Damarla <damarlasriharsha26@gmail.com>
2025-07-30 21:54:27 +0530

    Initial Commit

---
+++ src/main/java/termux/util/TermuxSmsGatewayApplication.java
@@ -0,0 +1,13 @@
+package termux.util;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TermuxSmsGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TermuxSmsGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/termux/util/TermuxSmsGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package termux.util;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TermuxSmsGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
